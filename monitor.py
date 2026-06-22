c292ea23f094a294e1920169335d47c46d61a185
Mason Proffitt <mason.proffitt@cern.ch>
2026-06-22 23:41:20 +0200

    improve histogram handling
    fill 1d histograms separately

--- monitor.py
+++ monitor.py
@@ -18,17 +18,17 @@ backtrack = True
 loop_delay = 1
 x_min = 0
 x_max = 10
-x_bins = 20
+x_bins = 100
 x_label = '$x$ [cm]'
 y_min = 0
 y_max = 10
-y_bins = 20
+y_bins = 100
 y_label = '$y$ [cm]'
 z_min = 0
 z_max = None
 z_label = f'Events per {(x_max - x_min) / x_bins} cm $\\times$ {(y_max - y_min) / y_bins} cm'
 histogram_plot_base_filename = 'test_plot_'
-histogram_plot_base_title = 'Point '
+histogram_plot_base_title = 'Station '
 histogram_plot_file_extension = '.png'
 coordinate_dtype = np.float32
 archive_histograms = True
@@ -58,8 +58,18 @@ tracker_file_mtimes = {}
 bins = [x_bins, y_bins]
 x_range = [x_min, x_max]
 y_range = [y_min, y_max]
-histogram_args = []
-histogram_kwargs = {
+x_histogram_args = []
+x_histogram_kwargs = {
+    'bins': x_bins,
+    'range': x_range,
+}
+y_histogram_args = []
+y_histogram_kwargs = {
+    'bins': y_bins,
+    'range': y_range,
+}
+two_d_histogram_args = []
+two_d_histogram_kwargs = {
     'bins': bins,
     'range': [x_range, y_range],
 }
@@ -108,28 +118,72 @@ def parse(path):
 
 def create_histograms():
     logging.debug('create histograms')
-    histograms = []
+    x_histograms = []
+    y_histograms = []
+    two_d_histograms = []
     for i in range(n_points):
-        histograms.append(list(np.histogram2d([], [], *histogram_args, **histogram_kwargs)))
-    logging.debug(f'{histograms=}')
-    return histograms
+        x_histograms.append(list(np.histogram([], *x_histogram_args, **x_histogram_kwargs)))
+        y_histograms.append(list(np.histogram([], *y_histogram_args, **y_histogram_kwargs)))
+        two_d_histograms.append(list(np.histogram2d([], [], *two_d_histogram_args, **two_d_histogram_kwargs)))
+    logging.debug(f'{x_histograms=} {y_histograms=} {two_d_histograms=}')
+    return [x_histograms, y_histograms, two_d_histograms]
 
 
 def add_to_histograms(coordinates, histograms):
     logging.debug('add to histograms')
     for i in range(n_points):
-        H = np.histogram2d(coordinates[:, n_coordinates_per_point * i], coordinates[:, n_coordinates_per_point * i + 1], *histogram_args, **histogram_kwargs)[0]
+        x_hist = np.histogram(coordinates[:, n_coordinates_per_point * i], *x_histogram_args, **x_histogram_kwargs)[0]
+        logging.debug(f'{i=} {x_hist=}')
+        histograms[0][i][0] += x_hist
+        y_hist = np.histogram(coordinates[:, n_coordinates_per_point * i + 1], *y_histogram_args, **y_histogram_kwargs)[0]
+        logging.debug(f'{i=} {y_hist=}')
+        histograms[1][i][0] += x_hist
+        H = np.histogram2d(coordinates[:, n_coordinates_per_point * i], coordinates[:, n_coordinates_per_point * i + 1], *two_d_histogram_args, **two_d_histogram_kwargs)[0]
         logging.debug(f'{i=} {H=}')
-        histograms[i][0] += H
+        histograms[2][i][0] += H
     return histograms
 
 
 def plot_histograms(histograms):
     logging.debug(f'plot {histograms=}')
     for i in range(n_points):
-        H = histograms[i][0].T
-        xedges = histograms[i][1]
-        yedges = histograms[i][2]
+        plt.title(histogram_plot_base_title + str(i + 1) + ', $x$')
+        plt.xlabel(x_label)
+        plt.ylabel(f'Events per {(x_max - x_min) / x_bins} cm')
+        # x_profile = H.sum(axis=0)
+        x_profile = histograms[0][i][0]
+        x_bin_edges = histograms[0][i][1]
+        x_dist = []
+        for j in range(len(x_profile)):
+            x_dist += [(x_bin_edges[j] + x_bin_edges[j + 1]) / 2] * int(x_profile[j])
+        x_dist = np.asarray(x_dist)
+        plt.stairs(x_profile, x_bin_edges, label=f'$\\mu$ = {x_dist.mean():.2f} $\\sigma$ = {x_dist.std():.2f}')
+        x_histogram_plot_filename = histogram_plot_base_filename + str(i + 1) + '_x' + histogram_plot_file_extension
+        plt.legend()
+        logging.info(f'save {x_histogram_plot_filename}')
+        plt.savefig(x_histogram_plot_filename)
+        plt.close()
+
+        plt.title(histogram_plot_base_title + str(i + 1) + ', $y$')
+        plt.xlabel(y_label)
+        plt.ylabel(f'Events per {(y_max - y_min) / y_bins} cm')
+        y_profile = histograms[1][i][0]
+        y_bin_edges = histograms[1][i][1]
+        # y_profile = H.sum(axis=1)
+        y_dist = []
+        for j in range(len(y_profile)):
+            y_dist += [(y_bin_edges[j] + y_bin_edges[j + 1]) / 2] * int(y_profile[j])
+        y_dist = np.asarray(y_dist)
+        plt.stairs(y_profile, y_bin_edges, label=f'$\\mu$ = {y_dist.mean():.2} $\\sigma$ = {y_dist.std():.2}')
+        y_histogram_plot_filename = histogram_plot_base_filename + str(i + 1) + '_y' + histogram_plot_file_extension
+        plt.legend()
+        logging.info(f'save {y_histogram_plot_filename}')
+        plt.savefig(y_histogram_plot_filename)
+        plt.close()
+
+        H = histograms[2][i][0].T
+        xedges = histograms[2][i][1]
+        yedges = histograms[2][i][2]
         X, Y = np.meshgrid(xedges, yedges)
         plt.title(histogram_plot_base_title + str(i + 1))
         plt.pcolormesh(X, Y, H)
@@ -137,39 +191,9 @@ def plot_histograms(histograms):
         plt.ylabel(y_label)
         plt.colorbar(label=z_label)
         plt.clim(z_min, z_max)
-        histogram_filename = histogram_plot_base_filename + str(i + 1) + histogram_plot_file_extension
-        logging.info(f'save {histogram_filename}')
-        plt.savefig(histogram_filename)
-        plt.close()
-
-        plt.title(histogram_plot_base_title + str(i + 1))
-        plt.xlabel(x_label)
-        plt.ylabel(f'Events per {(x_max - x_min) / x_bins} cm')
-        x_profile = H.sum(axis=0)
-        x_dist = []
-        for j in range(len(x_profile)):
-            x_dist += [(xedges[j] + xedges[j + 1]) / 2] * int(x_profile[j])
-        x_dist = np.asarray(x_dist)
-        plt.stairs(x_profile, xedges, label=f'$\\mu$ = {x_dist.mean():.2f} $\\sigma$ = {x_dist.std():.2f}')
-        histogram_filename = histogram_plot_base_filename + str(i + 1) + '_x' + histogram_plot_file_extension
-        plt.legend()
-        logging.info(f'save {histogram_filename}')
-        plt.savefig(histogram_filename)
-        plt.close()
-
-        plt.title(histogram_plot_base_title + str(i + 1))
-        plt.xlabel(y_label)
-        plt.ylabel(f'Events per {(y_max - y_min) / y_bins} cm')
-        y_profile = H.sum(axis=1)
-        y_dist = []
-        for j in range(len(y_profile)):
-            y_dist += [(yedges[j] + yedges[j + 1]) / 2] * int(y_profile[j])
-        y_dist = np.asarray(y_dist)
-        plt.stairs(y_profile, yedges, label=f'$\\mu$ = {y_dist.mean():.2} $\\sigma$ = {y_dist.std():.2}')
-        histogram_filename = histogram_plot_base_filename + str(i + 1) + '_y' + histogram_plot_file_extension
-        plt.legend()
-        logging.info(f'save {histogram_filename}')
-        plt.savefig(histogram_filename)
+        two_d_histogram_plot_filename = histogram_plot_base_filename + str(i + 1) + histogram_plot_file_extension
+        logging.info(f'save {two_d_histogram_plot_filename}')
+        plt.savefig(two_d_histogram_plot_filename)
         plt.close()
 
 
@@ -179,12 +203,17 @@ def copy_histograms():
         logging.info(f'creating {histogram_archive_directory_path}')
         histogram_archive_directory_path.mkdir()
     for i in range(n_points):
-        histogram_plot_filename = histogram_plot_base_filename + str(i) + histogram_plot_file_extension
-        histogram_plot_path = Path(histogram_plot_filename)
-        if histogram_plot_path.exists():
-            new_histogram_plot_filename = datetime.datetime.now().isoformat(timespec='seconds') + '_' + histogram_plot_filename
-            logging.info(f'copying {histogram_plot_filename} to {histogram_archive_directory_path / new_histogram_plot_filename}')
-            shutil.copy(histogram_plot_path, histogram_archive_directory_path / new_histogram_plot_filename)
+        x_histogram_plot_filename = histogram_plot_base_filename + str(i + 1) + '_x' + histogram_plot_file_extension
+        y_histogram_plot_filename = histogram_plot_base_filename + str(i + 1) + '_y' + histogram_plot_file_extension
+        two_d_histogram_plot_filename = histogram_plot_base_filename + str(i) + histogram_plot_file_extension
+        x_histogram_plot_path = Path(x_histogram_plot_filename)
+        y_histogram_plot_path = Path(y_histogram_plot_filename)
+        two_d_histogram_plot_path = Path(two_d_histogram_plot_filename)
+        for path in [x_histogram_plot_path, y_histogram_plot_path, two_d_histogram_plot_path]:
+            if path.exists():
+                new_histogram_plot_filename = datetime.datetime.now().isoformat(timespec='seconds') + '_' + path.name
+                logging.info(f'copying {path} to {histogram_archive_directory_path / new_histogram_plot_filename}')
+                shutil.copy(path, histogram_archive_directory_path / new_histogram_plot_filename)
 
 
 while True:
