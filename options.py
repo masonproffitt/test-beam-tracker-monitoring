0928ce9442f2184a27c77b432ea00549c99b343b
Mason Proffitt <mason.proffitt@cern.ch>
2026-06-23 14:40:25 +0200

    fix time zone

--- options.py
+++ options.py
@@ -1,1 +1,5 @@
+import zoneinfo
+
+
 run_start_time_filename = 'run_start_time'
+cern_time_zone = zoneinfo.ZoneInfo('Europe/Zurich')
--- reset_histograms.py
+++ reset_histograms.py
@@ -2,15 +2,11 @@
 
 import datetime
 import time
-import zoneinfo
 
 import options
 import utils
 
 
-cern_time_zone = zoneinfo.ZoneInfo('Europe/Zurich')
-
-
 reset_time = time.time()
 utils.write_run_start_time(reset_time)
-print(f'Reset tracker histograms at {datetime.datetime.fromtimestamp(reset_time).astimezone(cern_time_zone).ctime()}')
+print(f'Reset tracker histograms at {datetime.datetime.fromtimestamp(reset_time).astimezone(options.cern_time_zone).ctime()}')
