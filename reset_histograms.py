d4452a2f403d5676d2e8a6954e407fe8f287c05e
Mason Proffitt <mason.proffitt@cern.ch>
2026-06-23 08:41:52 +0200

    add confirmation message for histogram reset

--- reset_histograms.py
+++ reset_histograms.py
@@ -1,9 +1,18 @@
 #!/bin/env python3
 
+import datetime
 import time
+import zoneinfo
 
 import options
 
 
+cern_time_zone = zoneinfo.ZoneInfo('Europe/Zurich')
+
+
+reset_time = time.time()
+
 with open(options.run_start_time_filename, 'w') as f:
-    f.write(str(time.time()) + '\n')
+    f.write(str(reset_time) + '\n')
+
+print(f'Reset tracker histograms at {datetime.datetime.fromtimestamp(reset_time).astimezone(cern_time_zone).ctime()}')
