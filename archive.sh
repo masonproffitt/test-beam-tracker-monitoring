a001251942ebc72b7a823ab905efa88fd06ce837
Mason Proffitt <mason.proffitt@cern.ch>
2026-06-25 23:21:35 +0200

    add timestamp printouts to archive script

--- archive.sh
+++ archive.sh
@@ -3,6 +3,8 @@
 cd /home/daq/SiTrackerData
 while true
 do
+	echo Starting rsync at $(date)
 	rsync -hhav --progress --files-from=<(find ascii_dream_2026 datadir_dream_2026 -newermt "2026-06-22 12:00 CEST") ./ /data/HG-DREAM/CERN/TrackerData/all
+	echo Finished rsync at $(date)
 	sleep 3600
 done
