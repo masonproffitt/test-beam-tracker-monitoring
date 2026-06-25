94722beabe4c6c32559fc94668a56f0594e8ef0c
Mason Proffitt <mason.proffitt@cern.ch>
2026-06-25 23:17:39 +0200

    adjust minimun time for archived files

--- archive.sh
+++ archive.sh
@@ -3,6 +3,6 @@
 cd /home/daq/SiTrackerData
 while true
 do
-	rsync -hhav --progress --files-from=<(find ascii_dream_2026 datadir_dream_2026 -newermt "2026-06-22 08:00 CEST") ./ /data/HG-DREAM/CERN/TrackerData/all
+	rsync -hhav --progress --files-from=<(find ascii_dream_2026 datadir_dream_2026 -newermt "2026-06-22 12:00 CEST") ./ /data/HG-DREAM/CERN/TrackerData/all
 	sleep 3600
 done
